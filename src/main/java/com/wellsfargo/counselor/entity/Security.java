98554dfd07b2a04824721c1a557c5963c6021382
swarna <sw.uppada@gmail.com>
2024-01-04 23:02:09 -0500

    datamodel pojo classes security updated commit

--- src/main/java/com/wellsfargo/counselor/entity/Security.java
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -15,58 +15,58 @@ public class Security {
     private long securityId;
     private String name;
     private String category;
-    private LocalDate  purchaseDate = LocalDate.now();
-    private long purchasePrice;
-    private long quantity;
+    private final LocalDate  purchaseDate = LocalDate.now();
+    private  long purchasePrice;
+    private  long quantity;
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 
     private Portfolio portfolio;
+    public Security()
+    {
 
+    }
+    public Security(long securityId,String name,String category,LocalDate  purchaseDate,long purchasePrice,long quantity)
+    {
+        this.securityId = securityId;
+        this.name = name;
+        this.category = category;
+
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity;
+
+    }
     public long getSecurityId() {
         return securityId;
     }
 
-    public void setSecurityId(long securityId) {
-        this.securityId = securityId;
-    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+
 
     public String getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
-    }
+
 
     public LocalDate getPurchaseDate() {
         return purchaseDate;
     }
 
-    public void setPurchaseDate(LocalDate purchaseDate) {
-        this.purchaseDate = purchaseDate;
-    }
+
 
     public long getPurchasePrice() {
         return purchasePrice;
     }
 
-    public void setPurchasePrice(long purchasePrice) {
-        this.purchasePrice = purchasePrice;
-    }
+
 
     public long getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(long quantity) {
-        this.quantity = quantity;
-    }
+
 }
